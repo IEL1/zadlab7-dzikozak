23da5c85c056e930da827160a68785e87d886e04
dzikozak <dzikozak@tlen.pl>
2018-06-04 15:06:08 +0200

    zaległe laborki lab7

---
+++ OOPL7/src/main/java/pl/edu/ur/oopl7/zad2/Wyswietlanie.java
@@ -0,0 +1,15 @@
+package pl.edu.ur.oopl7.zad2;
+
+public class Wyswietlanie {
+    private static void as(){
+    Testowa testClass = new Testowa();
+        testClass.test2D();
+        testClass.test3D();
+        testClass.tablica2D();
+        testClass.tablica3D();
+        testClass.testCO();
+    }
+    public static void main(String[] args){
+        as();
+    }
+}
